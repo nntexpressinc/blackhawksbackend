cdbca3c0d6d6fc8b5065c7dcdff46190eeeda5c2
nntexpressinc <nntitdev@gmail.com>
2025-07-11 12:56:43 -0700

    updated signals

--- apps/load/ifta_signals.py
+++ apps/load/ifta_signals.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from apps.load.models.ifta import FuelTaxRate
+from apps.load.models.ifta import FuelTaxRate, Ifta
 from apps.load.models.driver import Driver
 
 
